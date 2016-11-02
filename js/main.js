cc36dd1adfd38912de4f7832b80e8dade72cbf6b
lichengzhu <coconutzhu@gmail.com>
2016-11-02 12:13:05 -0400

    updated modal

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
-$(function ()  {
+// $(function ()  {
     
-    $(".hamburger").click(function() {
-        var value = $(".content-section-b").offset().top;
-        console.log("Hamburger clicked: Offset: "+value);
-        $('html, body').animate({
-            scrollTop: $(".content-section-b").offset().top
-        }, 800);
+//     $(".hamburger").click(function() {
+//         var value = $(".content-section-b").offset().top;
+//         console.log("Hamburger clicked: Offset: "+value);
+//         $('html, body').animate({
+//             scrollTop: $(".content-section-b").offset().top
+//         }, 800);
 
-    });
+//     });
     
     
-});
+// });
+
+
+function scrollTo() {
+            $('html, body').animate({ scrollTop: $('#div_id').offset().top }, 'slow');
+            return false;
+        }
+
